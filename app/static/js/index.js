72249920afce1222e62d40095042be9216cfcf9f
Maoxie <edwardyzt@gmail.com>
2017-10-21 01:41:34 +0800

    add float-up animation for items in index page.

---
+++ app/static/js/index.js
@@ -0,0 +1,31 @@
+/**
+ * Created by yzt on 2017/10/20.
+ */
+// let float_up_items = $(".float-up");
+$(document).ready(function() {
+    $(".float-up").css({
+        "opacity": "0"
+    });
+    $(window).trigger("scroll");
+});
+$(window).scroll(function() {
+    $(".float-up").each(function(i) {
+        let top_of_object, bottom_of_object, bottom_of_window, top_of_window;
+        // bottom_of_object = $(this).position().top + $(this).outerHeight();
+        top_of_object = $(this).offset().top;
+        bottom_of_object = top_of_object + $(this).outerHeight();
+        top_of_window = $(window).scrollTop();
+        bottom_of_window = top_of_window + $(window).height();
+        let this_top = $(this).position().top;
+        if (bottom_of_window > top_of_object && top_of_window < bottom_of_object) {
+            $(this).css({
+               "top": this_top + 200
+            });
+            $(this).animate({
+                "opacity": "1",
+                "top": this_top
+            }, 1000);
+            $(this).removeClass("float-up");
+        }
+    })
+});
